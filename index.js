bc8f0fe3f51cb08e70f1904bf9991dcd942d5523
Elliot <elliotmrgn@gmail.com>
2020-04-18 10:42:11 -0400

    basic writing to file working

--- index.js
+++ index.js
@@ -16,21 +16,42 @@ function questions() {
             type: "input",
             name: "title",
             message: "What is your project's title?"
-        }
-
+        },
+         {
+            type: "input",
+            name: "description",
+            message: "What is your project's description?"
+        }/*,
+        {
+            type: "input",
+            name: "install",
+            message: "Describe your project's installation instructions?"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Describe your projects usage"
+        } */
     ]);
 }
 
 function writeToFile(fileName, data) {
-    
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        //file written successfully
+
+      })
 }
 
 async function init() {
     const answers = await questions();
-    console.log("init -> answers", answers)
-    
+    // const gitHub = api.getUser(answers.gitName)
+    // console.log("init -> gitHub", gitHub)
     const html = generateMarkdown(answers)
-    console.log("init -> html", html)
+    writeToFile("ReadMe.md", html)
 }
 
 init();
--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,14 @@
-const api = {
-  getUser(username) {
+const axios = require("../node_modules/axios")
 
+const api = {
+
+  getUser(username) {
+    axios.get(`https://api.github.com/users/${username}`)
+      .then((res) => {
+        // console.log("***************", res)
+        return res.data.avatar_url
+      })
+      return 
   }
 };
 
